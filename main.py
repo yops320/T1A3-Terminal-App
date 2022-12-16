ed55414146287719d8be0108c9a941f19dcfaad7
Liam <liam@Jades-MacBook-Air.local>
2022-12-16 20:24:12 +1030

    parameters

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import Title
 from colorama import Fore, Back, Style
 import time
 import menu
+import pandas as pd
 
-
-
+input_file = "recipe.csv"
+child_age = ["under 6 months", "6-12 months", "12-18months", "18-24months"]
+max_children = 4
+alloted_time = ["30 mins or less", "30-60 mins", "1-1.5hrs"]
 
 
 
