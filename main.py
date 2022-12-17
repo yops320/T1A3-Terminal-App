e650955d875d3f24a019b8cd78b94fb33ea89035
yops320 <liamdavidson4@gmail.com>
2022-12-17 12:34:39 +1030

    1

--- main.py
+++ main.py
@@ -9,37 +9,7 @@ child_age = ["under 6 months", "6-12 months", "12-18months", "18-24months"]
 max_children = 4
 alloted_time = ["30 mins or less", "30-60 mins", "1-1.5hrs"]
 
-input_file = "recipe.csv"
-child_age = ["under 6 months", "6-12 months", "12-18months", "18-24months"]
-max_children = 4
-alloted_time = ["30 mins or less", "30-60 mins", "1-1.5hrs"]
 
-def input_data():
-    data = []
-
-<<<<<<< HEAD
-    age = -1
-    while 1:
-        try:
-            age = int(input("Age of child. Please input the number(1-4)[ 1(" + child_age[0] + "), 2(" +
-                            child_age[1] + "), 3(" + child_age[2] + "), 4(" + child_age[3] + ") ] :   "))
-            if isinstance(age, int) and 1 <= age <= 4:
-                break
-        except:
-            continue
-    data.append(age)
-
-    N = -1
-    while 1:
-        try:
-            N = int(input("How many children(Please input the number between 1 and " + str(max_children) + ") :   "))
-            if isinstance(N, int) and 1 <= N <= max_children:
-                break
-        except:
-            continue
-    data.append(N)
-
-=======
 def input_data():
     data = []
 
@@ -64,7 +34,6 @@ def input_data():
             continue
     data.append(N)
 
->>>>>>> d7d849b7ff26e991e76f43e9bb1d3a795682f292
     time = -1
     while 1:
         try:
@@ -76,8 +45,4 @@ def input_data():
             continue
     data.append(time)
 
-<<<<<<< HEAD
     return data
-=======
-    return data
->>>>>>> d7d849b7ff26e991e76f43e9bb1d3a795682f292
