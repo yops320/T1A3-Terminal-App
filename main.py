5f291c4d0eebff8f0214e5874f85594962f0741c
yops320 <liamdavidson4@gmail.com>
2023-01-04 19:36:53 +1030

    add_data

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def input_data():
     age = -1
     while 1:
         try:
-            age = int(input("Age of child. Please input the number(1-4)[ 1(" + child_age[0] + "), 2(" +
-                            child_age[1] + "), 3(" + child_age[2] + "), 4(" + child_age[3] + ") ] :   "))
+            age = int(input("Age of child. Please input the number(1-4)[ 1(" + lst_age[0] + "), 2(" +
+                            lst_age[1] + "), 3(" + lst_age[2] + "), 4(" + lst_age[3] + ") ] :   "))
             if isinstance(age, int) and 1 <= age <= 4:
                 break
         except:
@@ -37,8 +37,8 @@ def input_data():
     time = -1
     while 1:
         try:
-            time = int(input("How much time do you have to cook? Please input the number(1-3)[ 1(" + alloted_time[0] +
-                             "), 2(" + alloted_time[1] + "), 3(" + alloted_time[2] + ") ]:   "))
+            time = int(input("How much time do you have to cook? Please input the number(1-3)[ 1(" + lst_time[0] +
+                             "), 2(" + lst_time[1] + "), 3(" + lst_time[2] + ") ]:   "))
             if isinstance(time, int) and 1 <= time <= 3:
                 break
         except:
@@ -47,6 +47,7 @@ def input_data():
 
     return data
 
+
 def main():
     print("===============start================")
 
@@ -54,19 +55,37 @@ def main():
 
     data = input_data()
 
-    age = child_age[data[0] - 1]
+    age = lst_age[data[0] - 1]
     N = data[1]
-    time = alloted_time[data[2] - 1]
-    for i in range(len(recipe_info["No"])):
-        if recipe_info[recipe_info.keys()[1]][i] == age and recipe_info[recipe_info.keys()[2]][i] == N and \
-                recipe_info[recipe_info.keys()[3]][i] == time:
-            print('---------For the following information : \n')
-            print('                   %s : %s\n' % (recipe_info.keys()[1], age))
-            print('                   %s : %d\n' % (recipe_info.keys()[2], N))
-            print('                   %s : %s\n' % (recipe_info.keys()[3], time))
-            print('---------Recipe No is : %d \n' % (recipe_info[recipe_info.keys()[4]][i]))
+    time = lst_time[data[2] - 1]
+    mathced_criteria_df = recipe_info[(recipe_info["Age of child"] == age) & (recipe_info["How many children"] == N) & (recipe_info["How much time do you have to cook"] == time)]
+    print("Following recipe(s) matched the criteria ", ', '.join(map(str, mathced_criteria_df['Recipe No'])))
+    recipe_number = 0
+    while True:
+        try:
+            recipe_number = int(input("Enter the recipe number to show "))
+            if recipe_number in mathced_criteria_df['Recipe No'].to_list():
+                break
+        except:
+            continue
+    print('---------For the following information : \n')
+    print('                   %s : %s\n' % (recipe_info.keys()[1], age))
+    print('                   %s : %d\n' % (recipe_info.keys()[2], N))
+    print('                   %s : %s\n' % (recipe_info.keys()[3], time))
+    print('---------Recipe No is : %d \n' % recipe_number)
 
+    while True:
+        add_rows = input('Do you want to add rows ? Yes/No ')
+        if add_rows == 'Yes':
+            data = input_data()
+            recipe_number = input('Enter Recipe Number : ')
+            recipe_info.loc[len(recipe_info)] = [recipe_info['No'].iloc[len(recipe_info)-1] + 1, lst_age[data[0]], data[1], lst_time[data[2]], recipe_number]
+            print('Recipe has been added ! ')
+        elif add_rows == 'No':
             break
+        else:
+            continue
+    recipe_info.to_csv('recipe.csv', index=False)
 
     print("===============end================")
 
